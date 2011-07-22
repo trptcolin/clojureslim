28a1356a22d1621256a9de95854d2d8fa42caccd
Colin Jones <trptcolin@gmail.com>
2011-07-22 16:32:27 -0500

    Update fitnesse dev-dependency to clojars

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "The Slim (Simple List Invocation Method) protocol implented for Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[speclj "1.4.0"]
-                     [org.fitnesse/fitnesse "20110721"]]
+                     [org.clojars.trptcolin/fitnesse "20110721"]]
   :test-path "spec")
