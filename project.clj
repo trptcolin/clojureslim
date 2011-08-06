b32e72bd966f91d755b822430c321d959121e882
Colin Jones <trptcolin@gmail.com>
2011-08-06 13:52:39 -0500

    Update to fitnesse version with longer timeout

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojureslim "0.0.1-SNAPSHOT"
   :description "The Slim (Simple List Invocation Method) protocol implented for Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojars.trptcolin/fitnesse "20110721"]]
+                 [org.clojars.trptcolin/fitnesse "20110806"]]
   :dev-dependencies [[speclj "1.4.0"]]
   :dev-resources-path "examples"
   :test-path "spec")
